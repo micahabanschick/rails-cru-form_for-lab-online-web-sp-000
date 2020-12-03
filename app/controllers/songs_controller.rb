d70c71414abc644c0605d4b1929acc2a5f9e57a2
Micah Banschick <micha.ban@gmail.com>
2020-12-03 19:23:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,22 +1,38 @@
 class SongsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  # add edit and update methods here
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name))
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
 end
