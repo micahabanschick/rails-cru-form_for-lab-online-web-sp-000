0ea3e3db711a2f5d397e997fd872923a20569f08
Micah Banschick <micha.ban@gmail.com>
2020-12-03 20:01:08 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -25,7 +25,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(artist_params(:bio))
+    @artist.update(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
 
