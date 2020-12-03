bb8dc708054e41c655ba0df6772956f18693a18f
Micah Banschick <micha.ban@gmail.com>
2020-12-03 16:41:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,48 @@
 Rails.application.routes.draw do
+  get 'genres/new'
+
+  get 'genres/create'
+
+  get 'genres/edit'
+
+  get 'genres/update'
+
+  get 'genres/destroy'
+
+  get 'genres/index'
+
+  get 'genres/show'
+
+  resources :genres
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
+  get 'artists/destroy'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  resources :artists
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/edit'
+
+  get 'songs/update'
+
+  get 'songs/destroy'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
