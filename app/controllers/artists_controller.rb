418cdd9c893210796532c1f16d6dac9b29e9f6f8
Micah Banschick <micha.ban@gmail.com>
2020-12-03 19:17:48 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,22 +1,38 @@
 class ArtistsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:title, :room_number))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  # add edit and update methods here
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:title))
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
+
 end
